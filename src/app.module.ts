681533d3a1c4078a50ba9e40f35024a15add9348
Rafael Camillo <camillodeveloper@gmail.com>
2025-01-30 23:43:55 -0300

    adding database url

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
+
+    MongooseModule.forRoot(`${process.env.DATABASE_URL}`),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
