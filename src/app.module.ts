865ab0aeb7c9f113c4e567af598cd885361adbfd
Rafael Camillo <camillodeveloper@gmail.com>
2025-01-30 23:59:39 -0300

    fix imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CleanedDataModule } from './cleaned-data/cleaned-data.module';
-import { TransformationLogModule } from './transformation-log/transformation-log.module';
-import { CleanedDataclearController } from './cleaned-dataclear/cleaned-dataclear.controller';
-import { IngestionModule } from './ingestion/ingestion.module';
 import { DataAccessModule } from './data-access/data-access.module';
-import { IngestionModule } from './ingestion/ingestion.module';
 import * as dotenv from 'dotenv';
+import { IngestionModule } from './ingestion/ingestion.module';
 
 dotenv.config();
 
@@ -17,12 +13,10 @@ dotenv.config();
   imports: [
     ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
     MongooseModule.forRoot(`${process.env.DATABASE_URL}`),
-    CleanedDataModule,
-    TransformationLogModule,
     IngestionModule,
     DataAccessModule,
   ],
-  controllers: [AppController, CleanedDataclearController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
